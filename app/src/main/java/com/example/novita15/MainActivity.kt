18960eae4fd0b3de4b790ff94b13a7796cf7fec1
zuliana21 <zuli25176@gmail.com>
2022-11-16 08:38:24 +0700

    first commit

---
+++ app/src/main/java/com/example/novita15/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.novita15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btnnama = findViewById<Button>(R.id.nama)
+        val btnkelas = findViewById<Button>(R.id.kelas)
+        val btnnis = findViewById<Button>(R.id.nis)
+
+        btnnama.setOnClickListener {
+            Toast.makeText( this,  "Novita Sari", Toast.LENGTH_SHORT).show()
+        }
+        btnkelas.setOnClickListener {
+            Toast.makeText( this,  "XI PPLG 1", Toast.LENGTH_SHORT).show()
+        }
+        btnnis.setOnClickListener {
+            Toast.makeText( this,  "3022", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
